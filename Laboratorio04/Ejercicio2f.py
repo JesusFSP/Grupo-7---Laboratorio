c8dafe1dcc6054c159f8f1966e78fbe54dddd52b
Gustavo Linares <glinares@unsa.edu.pe>
2026-05-07 01:41:36 -0500

    Mejorando ejercicio f

--- Laboratorio04/Ejercicio2f.py
+++ Laboratorio04/Ejercicio2f.py
@@ -4,6 +4,7 @@ from chessPictures import *
 fila1 = square.join(square.negative()).horizontalRepeat(4)
 fila2 = square.negative().join(square).horizontalRepeat(4)
 
-tablero = fila1.under(fila2).verticalRepeat(4)
+dos_filas = fila2.up(fila1)
+tablero = dos_filas.verticalRepeat(4)
 
 draw(tablero)
