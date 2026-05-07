01299206f0d8bf472eae92d69ccf39e526ab1afd
Gustavo Linares <glinares@unsa.edu.pe>
2026-05-06 23:45:24 -0500

    Ejercicio f completado

--- Laboratorio04/Ejercicio2b.py
+++ Laboratorio04/Ejercicio2b.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+fila1 = square.join(square.negative()).horizontalRepeat(4)
+fila2 = square.negative().join(square).horizontalRepeat(4)
+
+tablero = fila1.under(fila2).verticalRepeat(4)
+
+draw(tablero)
