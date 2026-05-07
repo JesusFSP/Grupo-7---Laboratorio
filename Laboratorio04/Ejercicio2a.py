2e6a813c3e07ac80919e024a718c8cd4db2c4855
Gustavo Linares <glinares@unsa.edu.pe>
2026-05-06 23:16:28 -0500

    Agregando ejercicio a caballos 2x2 colores alternos

--- Laboratorio04/Ejercicio2a.py
+++ Laboratorio04/Ejercicio2a.py
@@ -1,4 +1,9 @@
 from interpreter import draw
 from chessPictures import *
 
-draw(rock)
+fila1 = knight.join(knight.negative())
+fila2 = knight.negative().join(knight)
+
+figura = fila1.under(fila2)
+
+draw(figura)
