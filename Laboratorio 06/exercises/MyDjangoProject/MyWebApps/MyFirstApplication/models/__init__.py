9de7eb1b7ebc9f63359aef9b417ee491477b65dc
JesusFSP <jsilva@unsa.edu.pe>
2026-05-28 00:36:13 -0500

    Creando los modelos del proyecto, y agregandolos al init

---
+++ Laboratorio 06/exercises/MyDjangoProject/MyWebApps/MyFirstApplication/models/__init__.py
@@ -0,0 +1,10 @@
+from .BaseModel import BaseModel
+from .Customer import Customer
+from .Table import Table
+from .MenuCategory import MenuCategory
+from .MenuItem import MenuItem
+from .Reservation import Reservation
+
+__all__ = [
+    "BaseModel", "Customer", "Table", "MenuCategory", "MenuItem", "Reservation"
+]
