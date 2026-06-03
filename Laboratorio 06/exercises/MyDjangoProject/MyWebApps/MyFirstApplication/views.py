7f781e9b836bad2c02a2d560864cc30cd26aceda
Gustavo Linares <glinares@unsa.edu.pe>
2026-06-03 18:42:16 -0500

    Mostrar menu del restaurante desde la base de datos

--- Laboratorio 06/exercises/MyDjangoProject/MyWebApps/MyFirstApplication/views.py
+++ Laboratorio 06/exercises/MyDjangoProject/MyWebApps/MyFirstApplication/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import MenuItem
 
-# Create your views here.
 def home(request):
     return render(request, 'MyFirstApplication/home.html')
+
+def menu_list(request):
+    items = MenuItem.objects.all()
+    return render(
+        request,
+        'MyFirstApplication/menu_list.html',
+        {'items': items}
+    )
