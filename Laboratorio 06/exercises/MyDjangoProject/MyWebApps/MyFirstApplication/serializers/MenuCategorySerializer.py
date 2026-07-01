3c90afd10689a0ae8eb6c2492dc6dc2c8c0cf2e5
GusLinares17 <glinares@unsa.edu.pe>
2026-07-01 05:01:14 -0500

    Delete Laboratorio 06/exercises/MyDjangoProject/MyWebApps/MyFirstApplication/serializers directory

--- Laboratorio 06/exercises/MyDjangoProject/MyWebApps/MyFirstApplication/serializers/MenuCategorySerializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ..models import MenuCategory
-
-
-class MenuCategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = MenuCategory
-        fields = '__all__'
